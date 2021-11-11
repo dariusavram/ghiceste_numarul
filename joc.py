dac322d11447abbd628ab596355916c92fd7a2e1
Nostravramus-hub <82176358+Nostravramus-hub@users.noreply.github.com>
2021-11-11 13:45:23 +0200

    Update joc.py

--- joc.py
+++ joc.py
@@ -1,9 +1,18 @@
 from random import *
-print("Ghiceste numarul: ")
-a=randrange(10000)
+print("Ghicește numărul: ")
+a=randrange(100)
+k=5
 while a:
     b=int(input())
     if b==a:
-        print("Ai ghicit numarul!")
+        print('Ai ghicit numărul!')
         break;
-    print("Mai mare" if (int(b)<a) else "Mai mic")
+    print('\nMai mare' if (int(b)<a) else '\nMai mic')
+    k-=1
+    if k>1:
+        print('\nMai ai '+str(k)+' vieți\n')
+    elif k==1:
+            print('\nMai ai o viață!')
+    if k==0:
+        print("Ai pierdut!")
+        break;
